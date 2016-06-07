95b46d187b2a833b8c42fe8d666165a23c04b23c
Jonathan Wright <jon@than.io>
2016-06-07 21:57:40 +0100

    Make sure that EC2 instances are always destroyed

--- scripts/bash/kitchen-test.sh
+++ scripts/bash/kitchen-test.sh
@@ -4,8 +4,9 @@ GEM_HOME="$(ruby -e 'print Gem.user_dir' 2> /dev/null)"
 PATH=${GEM_HOME}/bin:${PATH}
 export GEM_HOME PATH
 
+echo
 kitchen version
 echo
 kitchen list
 echo
-kitchen test
+kitchen test --destroy always
