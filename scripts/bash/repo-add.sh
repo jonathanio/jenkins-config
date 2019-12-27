f1f14cceafb98b26aeee9692dd796166a549ff76
Jonathan Wright <jon@than.io>
2019-12-27 14:49:54 +0000

    Add support for *.zstd packages and race conditions
    Fix issue with race conditions where once package can add the files of
    another package, including when the file is still being uploaded. This
    can break the build of the other package if it's missing.

--- scripts/bash/repo-add.sh
+++ scripts/bash/repo-add.sh
@@ -1,16 +1,33 @@
 #!/usr/bin/env bash -ex
 
-cd private/new
-for PKG in *.pkg.tar.xz *.pkg.tar.zst
+PKG_DIR=${JOB_BASE_NAME}
+REPO=$(pwd)/private
+
+cd ${REPO}/new/${PKG_DIR//-auto/}
+
+echo "Loading packages in ${PKG_DIR//-auto}:"
+find \
+  -maxdepth 1 \
+      -iname '*.pkg.tar.zstd' \
+  -or -iname '*.pkg.tar.xz' \
+  -type f -printf '%f\n' | \
+while read PKG
 do
-  if [[ -e "../${PKG}" ]]; then
-    mv "${PKG}" "../${PKG}"
-    # Forcefully update and override existing version
-    ( cd .. && repo-add -R private.db.tar.gz "${PKG}" )
-  else
-    mv "${PKG}" "../${PKG}"
-    # Upgrade current version
-    ( cd .. && repo-add -n -R private.db.tar.gz "${PKG}" )
+  ADD_OPTIONS="-R"
+
+  echo "Loading package ${PKG}"
+
+  if [[ ! -e "${REPO}/${PKG}" ]]
+  then
+    # Upgrade current version, not just override existing
+    ADD_OPTIONS+=" -n"
   fi
+
+  mv "${PKG}" "${REPO}/${PKG}"
+  ( cd "${REPO}" && \
+    set -x && \
+    repo-add ${ADD_OPTIONS} private.db.tar.gz "${PKG}" )
 done
 
+# Delete anything that hasn't been moved and added
+rm -f *
